636c653b6ab86aefd7e2c76e186343b6cd548d76
Hot Dominik <forro.dominik.adrian@diak.szbi-pg.hu>
2026-02-11 09:25:51 +0100

    Update labdarugo_jatekosok.py

--- labdarugo_jatekosok.py
+++ labdarugo_jatekosok.py
@@ -10,14 +10,40 @@ Olvasd be a labdarugok.txt adatait, majd oldd meg az alábbi feladatokat!
 ***EXTRA - nehezebb feladat*** (nem kötelező, de érdemes megpróbálni):
 6. Melyik csapat szerzett a legtöbb gólt? (feltételezve, hogy egy játékos csak egy csapatban játszott)
 
-
+#Név; Csapat; Gólszám; Mérkőzések száma
 A megoldott feladatokat a kiirt_adatok nevű mappában hozd létre statisztika.txt néven!
 """
+labdarugok = []
+with open("beolvasando_adatok/labdarugok.txt", "r", encoding="UTF8") as forrasfajl:
+    next(forrasfajl)
+    for sor in forrasfajl:
+        sor = sor.rstrip("\n")
+        labdarugok.append(list(sor.split(";")))
+    for i in range(len(labdarugok)):
+        labdarugok[i][2] = int(labdarugok[i][2])
+        labdarugok[i][3] = int(labdarugok[i][3])
 
+def legkevesebb_gol(labdarugok):
+    golok_szama = 100000000000 
+    jatekos = ""
+    for i in range(len(labdarugok)):
+        if golok_szama > labdarugok[i][2]:
+            golok_szama = labdarugok[i][2]
+            jatekos = labdarugok[i][0]
+    return jatekos
 
-print("A beolvasott fájlban összesen ____ játékos szerepel.")
-print("A legkevesebb gólt szerző játékos: ____")
-print("A legtöbb gólt szerző játékos: ____")
-print("A legtöbb mérkőzést játszó játékos: ____")
-print("Az átlagos gólszám: ____")
-print("***A legtöbb gólt szerző csapat: ____")
+def legtobb_gol(labdarugok):
+    golok_szama = 0
+    jatekos = ""
+    for i in range(len(labdarugok)):
+        if golok_szama < labdarugok[i][2]:
+            golok_szama = labdarugok[i][2]
+            jatekos = labdarugok[i][0]
+    return jatekos
+
+print(f"A beolvasott fájlban összesen {legkevesebb_gol(labdarugok)} játékos szerepel.")
+print(f"A legkevesebb gólt szerző játékos: {legkevesebb_gol(labdarugok)}")
+print(f"A legtöbb gólt szerző játékos: ____")
+print(f"A legtöbb mérkőzést játszó játékos: ____")
+print(f"Az átlagos gólszám: ____")
+print(f"***A legtöbb gólt szerző csapat: ____")
